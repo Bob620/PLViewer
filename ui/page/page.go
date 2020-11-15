cdcc90c2ab4e889a01a5a81e4cd73d3735c0e32d
Bob620 <bruder.kraft225@gmail.com>
2020-11-15 05:29:48 -0600

    feat(page) created a page view

--- ui/page/page.go
+++ ui/page/page.go
@@ -6,17 +6,63 @@ import (
 )
 
 type Page struct {
-	Title string
-	Id    string
-	grid  *tview.Grid
+	Title      string
+	Id         string
+	grid       *tview.Grid
+	active     bool
+	onActivate func()
+	selected   bool
+	onSelect   func()
+	onDeselect func()
 }
 
 func MakePage(title string) *Page {
 	return &Page{
 		Title: title,
 		Id:    title,
-		grid:  tview.NewGrid().AddItem(tview.NewTextView().SetText(title), 0, 0, 1, 1, 0, 0, true),
+		grid:  tview.NewGrid(),
 	}
+}
+
+func (page *Page) SetOnActivate(handler func()) {
+	page.onActivate = handler
+}
+
+func (page *Page) GetActive() bool {
+	return page.active
+}
+
+func (page *Page) Activate(active bool) {
+	page.active = active
+	if page.onActivate != nil {
+		page.onActivate()
+	}
+}
+
+func (page *Page) SetOnSelect(handler func()) {
+	page.onSelect = handler
+}
+
+func (page *Page) IsSelected() bool {
+	return page.selected
+}
+
+func (page *Page) Select() {
+	page.selected = true
+	if page.onSelect != nil {
+		page.onSelect()
+	}
+}
+
+func (page *Page) SetOnDeselect(handler func()) {
+	page.onDeselect = handler
+}
+
+func (page *Page) Deselect() {
+	if page.onDeselect != nil {
+		page.onDeselect()
+	}
+	page.selected = false
 }
 
 func (page *Page) Grid() *tview.Grid {
